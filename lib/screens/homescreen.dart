22d881a415540e69c503dfd99ede5779a64ff4f2
sumitvarun <sumit.varun@rocketmail.com>
2021-11-14 16:22:46 +0530

    Update

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -4,6 +4,7 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:flutter/src/material/colors.dart';
@@ -88,7 +89,7 @@ class _homescreenState extends State<homescreen> {
             ),
             ElevatedButton(
               child: Text("Share"),
-              onPressed: () async {},
+              onPressed: _captureAndSharePng,
             )
           ],
         ),
@@ -109,5 +110,26 @@ class _homescreenState extends State<homescreen> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
     );
+  }
+}
+
+final globalKey = GlobalKey();
+
+Future<void> _captureAndSharePng() async {
+  try {
+    RenderRepaintBoundary boundary =
+        globalKey.currentContext.findRenderObject();
+    var image = await boundary.toImage();
+    ByteData byteData = await image.toByteData(format: ImageByteFormat.png);
+    Uint8List pngBytes = byteData.buffer.asUint8List();
+
+    final tempDir = await getTemporaryDirectory();
+    final file = await new File('${tempDir.path}/image.png').create();
+    await file.writeAsBytes(pngBytes);
+
+    final channel = const MethodChannel('channel:me.alfian.share/share');
+    channel.invokeMethod('shareFile', 'image.png');
+  } catch (e) {
+    print(e.toString());
   }
 }
