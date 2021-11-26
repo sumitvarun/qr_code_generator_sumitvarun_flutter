3ac784f488391bcde5765017284493a3640a2940
sumitvarun <sumit.varun@rocketmail.com>
2021-11-26 21:52:20 +0530

    Update

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -7,43 +7,38 @@ class WelcomeScreen extends StatelessWidget {
     return MaterialApp(
       title: 'QR Code Generator',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('QR Code Generator'),
-        ),
         body: Center(
-          child: Row(
-            children: <Widget>[
-              Container(
-                  width: 200.0,
-                  padding: EdgeInsets.only(top: 02.0, bottom: 02.0),
-                  child: TextButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                          Colors.redAccent[400]),
-                      foregroundColor:
-                          MaterialStateProperty.all<Color>(Colors.white),
-                      // shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      //  RoundedRectangleBorder(
-                      //      borderRadius: BorderRadius.circular(18.0),
-                      //      side: BorderSide(color: Colors.black)),
-                      // ),
-                      overlayColor: MaterialStateProperty.all<Color>(
-                          Colors.blueAccent[400]),
-                    ),
-                    // Within the `FirstRoute` widget
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => HomeScreen()),
-                      );
-                    },
-                    child: Text("Generate QR Code "),
-                  ),
+            child: Row(
+          children: <Widget>[
+            Container(
+              width: 200.0,
+              padding: EdgeInsets.only(top: 02.0, bottom: 02.0),
+              child: TextButton(
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.redAccent[400]),
+                  foregroundColor:
+                      MaterialStateProperty.all<Color>(Colors.white),
+                  // shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  //  RoundedRectangleBorder(
+                  //      borderRadius: BorderRadius.circular(18.0),
+                  //      side: BorderSide(color: Colors.black)),
+                  // ),
+                  overlayColor:
+                      MaterialStateProperty.all<Color>(Colors.blueAccent[400]),
                 ),
-            ],
-          )
-          ),
-        ),
+                // Within the `FirstRoute` widget
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HomeScreen()),
+                  );
+                },
+                child: Text("Welcome"),
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
