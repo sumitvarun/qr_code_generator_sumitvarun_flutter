89386a3697816005b598e83964ed90663ef8978f
sumitvarun <sumit.varun@rocketmail.com>
2021-11-26 15:19:49 +0530

    Update

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -57,10 +57,23 @@ class HomeScreen extends StatelessWidget {
             child: Column(
               children: <Widget>[
                 Container(
-                  
                   padding: EdgeInsets.only(top: 40.0, bottom: 20.0),
                   child: TextButton(
-                    style: ,
+                    style: ButtonStyle(
+                      backgroundColor: ,
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                      overlayColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.hovered))
+                            return Colors.blue.withOpacity(0.04);
+                          if (states.contains(MaterialState.focused) ||
+                              states.contains(MaterialState.pressed))
+                            return Colors.blue.withOpacity(0.12);
+                          return null; // Defer to the widget's default.
+                        },
+                      ),
+                    ),
                     // Within the `FirstRoute` widget
                     onPressed: () {
                       Navigator.push(
