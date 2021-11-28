248074bf81ea1abe8c55fa75bd7757d564a5b86d
sumitvarun <sumit.varun@rocketmail.com>
2021-11-29 00:36:20 +0530

    Update

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -25,20 +25,44 @@ class _SplashScreenState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image(
-              image: AssetImage("lib/asset/QR GEN-logos/QR GEN-logos.jpeg"),
-              height: 200,
-              width: 200,
-              fit: BoxFit.cover,
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Image(
+                  image: AssetImage("lib/asset/QR GEN-logos/QR GEN-logos.jpeg"),
+                  height: 200,
+                  width: 200,
+                  fit: BoxFit.cover,
+                ),
+                Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Image(
+                        image:
+                            AssetImage('lib/asset/SUMITVARUN-logos_black.png'),
+                        height: 70,
+                        width: 70,
+                      ),
+                      Text(
+                        'SUMITVARUN',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 10,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
